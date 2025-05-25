7f7ccb2686cd5c4932a403e42c5f3f22191542e2
Kiraffi <10407365+Kiraffi@users.noreply.github.com>
2025-05-25 13:00:50 +0300

    Rearrange some of the code

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,15 @@
 #include <raylib.h>
 #include <raymath.h>
 
+static const float ROTATION_SPEED = 4.0;
+
+static const float PHYSICS_TIME_STEP = 1.0 / 64.0;
+static const float SPEED = 256.0;
+static const float BULLET_SPEED = 512.0;
+static const float BULLET_MAX_TIME = 2.0;
+
+static const float MAX_ROCK_SPEED = 128.0;
+
 
 enum Team : uint8_t
 {
@@ -72,8 +81,18 @@ static void listSwapRemove(T& vec, int index)
 static void init();
 static void deinit();
 static void update();
+
 static void updatePhysics();
+static void updateShip(double dt);
+static void updateBullets(double dt);
+static void updateRocks(double dt);
+
 static void render();
+static void drawGrid(int32_t screenWidth, int32_t screenHeight);
+static void drawPlayer(int32_t screenWidth, int32_t screenHeight);
+static void drawBullets(int32_t screenWidth, int32_t screenHeight);
+static void drawRocks(int32_t screenWidth, int32_t screenHeight);
+
 
 static float randomFloat()
 {
@@ -90,11 +109,6 @@ int main(void)
         update();
         updatePhysics();
         render();
-
-        //BeginDrawing();
-        //ClearBackground(RAYWHITE);
-        //DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        //EndDrawing();
     }
 
     deinit();
@@ -118,15 +132,7 @@ static void deinit()
 
 static void update()
 {
-    double dt = GetFrameTime();
-
-    static const float ROTATION_SPEED = 4.0;
-
-    if(IsKeyDown(KEY_A)) g_vars.player.rotation -= dt * ROTATION_SPEED;
-    if(IsKeyDown(KEY_D)) g_vars.player.rotation += dt * ROTATION_SPEED;
-
-    while(g_vars.player.rotation >= 2.0 * PI) g_vars.player.rotation -= 2.0 * PI;
-    while(g_vars.player.rotation <= 0.0 * PI) g_vars.player.rotation += 2.0 * PI;
+    //double dt = GetFrameTime();
 }
 
 static void updatePhysics()
@@ -134,20 +140,7 @@ static void updatePhysics()
     double dt = GetFrameTime();
     g_vars.dtOver += dt;
 
-    static float PHYSICS_TIME_STEP = 1.0 / 64.0;
-
     int32_t physicsSteps = 0;
-
-    float c = std::cos(g_vars.player.rotation);
-    float s = std::sin(g_vars.player.rotation);
-
-    Vector2 forward = {c, s};
-
-    static float SPEED = 256.0;
-    static float BULLET_SPEED = 512.0;
-    static float BULLET_MAX_TIME = 2.0;
-
-    static float MAX_ROCK_SPEED = 1.0;
 
     float screenWidth = GetScreenWidth();
     float screenHeight = GetScreenHeight();
@@ -161,83 +154,161 @@ static void updatePhysics()
         g_vars.physicsTimeStep += 1;
         physicsSteps += 1;
 
-        g_vars.player.shootCD = std::max(0.0f, g_vars.player.shootCD - PHYSICS_TIME_STEP);
-
-        if(IsKeyDown(KEY_W)) g_vars.player.speed +=  forward * PHYSICS_TIME_STEP * SPEED;
-        if(IsKeyDown(KEY_S)) g_vars.player.speed -= forward * PHYSICS_TIME_STEP * SPEED;
-        if(IsKeyDown(KEY_SPACE) && g_vars.player.shootCD <= 0.0)
-        {
-            g_vars.player.shootCD = 0.25;
-            bullets.emplace_back(Bullet{
-                .pos = g_vars.player.pos,
-                .speed = Vector2{c, s} * BULLET_SPEED + g_vars.player.speed,
-                .time = 0.0,
-                .team = PlayerTeam
-            });
-        }
-
-        g_vars.player.speed *= 0.99;
-        g_vars.player.pos += g_vars.player.speed * PHYSICS_TIME_STEP;
-
-        while(g_vars.player.pos.x >= screenWidth * 0.5) g_vars.player.pos.x -= screenWidth;
-        while(g_vars.player.pos.x <= -screenWidth * 0.5) g_vars.player.pos.x += screenWidth;
-        while(g_vars.player.pos.y >= screenHeight * 0.5) g_vars.player.pos.y -= screenHeight;
-        while(g_vars.player.pos.y <= -screenHeight * 0.5) g_vars.player.pos.y += screenHeight;
-
-        for(int i = bullets.size() - 1; i >= 0; --i)
-        {
-            Bullet& b = bullets[i];
-            b.pos += b.speed * PHYSICS_TIME_STEP;
-            b.time += PHYSICS_TIME_STEP;
-            if(b.time >= BULLET_MAX_TIME)
-            {
-                listSwapRemove(bullets, i);
-                continue;
-            }
-            while(b.pos.x >= screenWidth * 0.5)   b.pos.x -= screenWidth;
-            while(b.pos.x <= -screenWidth * 0.5)  b.pos.x += screenWidth;
-            while(b.pos.y >= screenHeight * 0.5)  b.pos.y -= screenHeight;
-            while(b.pos.y <= -screenHeight * 0.5) b.pos.y += screenHeight;
-
-        }
-        if(rocks.size() < 10)
-        {
-            rocks.emplace_back(Rock{
-                .pos = Vector2{w * randomFloat(), h * randomFloat()},
-                .speed = Vector2{
-                    (randomFloat() * 2.0f - 1.0f),
-                    (randomFloat() * 2.0f - 1.0f)} * MAX_ROCK_SPEED
-            });
-        }
-
-        for(int i = rocks.size() - 1; i >= 0; --i)
-        {
-            Rock& r = rocks[i];
-            r.pos += r.speed * PHYSICS_TIME_STEP;
-            while(r.pos.x >= screenWidth * 0.5)   r.pos.x -= screenWidth;
-            while(r.pos.x <= -screenWidth * 0.5)  r.pos.x += screenWidth;
-            while(r.pos.y >= screenHeight * 0.5)  r.pos.y -= screenHeight;
-            while(r.pos.y <= -screenHeight * 0.5) r.pos.y += screenHeight;
-
-            for(const Bullet& b : bullets)
-            {
-                Vector2 diff = r.pos - b.pos;
-                float d = Vector2DotProduct(diff, diff);
-                if(d < 25.0f)
-                {
-                    listSwapRemove(rocks, i);
-                    break;
-                }
-            }
-
-        }
+        updateShip(PHYSICS_TIME_STEP);
+        updateBullets(PHYSICS_TIME_STEP);
+        updateRocks(PHYSICS_TIME_STEP);
     }
     std::string str = "Pos: ";
     str += std::to_string(g_vars.player.pos.x);
     str += ", ";
     str += std::to_string(g_vars.player.pos.y);
     DrawText(str.c_str(), 10, 30, 16, RAYWHITE);
+}
 
+static void updateShip(double dt)
+{
+    float screenWidth = GetScreenWidth();
+    float screenHeight = GetScreenHeight();
+
+    if(IsKeyDown(KEY_A)) g_vars.player.rotation -= dt * ROTATION_SPEED;
+    if(IsKeyDown(KEY_D)) g_vars.player.rotation += dt * ROTATION_SPEED;
+
+    while(g_vars.player.rotation >= 2.0 * PI) g_vars.player.rotation -= 2.0 * PI;
+    while(g_vars.player.rotation <= 0.0 * PI) g_vars.player.rotation += 2.0 * PI;
+
+    float c = std::cos(g_vars.player.rotation);
+    float s = std::sin(g_vars.player.rotation);
+
+    Vector2 forward = {c, s};
+
+    g_vars.player.shootCD = std::max(0.0f, g_vars.player.shootCD - float(dt));
+
+    if(IsKeyDown(KEY_W)) g_vars.player.speed +=  forward * dt * SPEED;
+    if(IsKeyDown(KEY_S)) g_vars.player.speed -= forward * dt * SPEED;
+
+    g_vars.player.speed *= 0.99;
+    g_vars.player.pos += g_vars.player.speed * dt;
+
+    while(g_vars.player.pos.x >= screenWidth * 0.5) g_vars.player.pos.x -= screenWidth;
+    while(g_vars.player.pos.x <= -screenWidth * 0.5) g_vars.player.pos.x += screenWidth;
+    while(g_vars.player.pos.y >= screenHeight * 0.5) g_vars.player.pos.y -= screenHeight;
+    while(g_vars.player.pos.y <= -screenHeight * 0.5) g_vars.player.pos.y += screenHeight;
+}
+
+static void updateBullets(double dt)
+{
+    float screenWidth = GetScreenWidth();
+    float screenHeight = GetScreenHeight();
+
+    float c = std::cos(g_vars.player.rotation);
+    float s = std::sin(g_vars.player.rotation);
+    Vector2 forward = {c, s};
+
+    if(IsKeyDown(KEY_SPACE) && g_vars.player.shootCD <= 0.0)
+    {
+        g_vars.player.shootCD = 0.25;
+        bullets.emplace_back(Bullet{
+            .pos = g_vars.player.pos,
+            .speed = forward * BULLET_SPEED + g_vars.player.speed,
+            .time = 0.0,
+            .team = PlayerTeam
+        });
+    }
+
+    for(int i = bullets.size() - 1; i >= 0; --i)
+    {
+        Bullet& b = bullets[i];
+        b.pos += b.speed * dt;
+        b.time += dt;
+        if(b.time >= BULLET_MAX_TIME)
+        {
+            listSwapRemove(bullets, i);
+            continue;
+        }
+        while(b.pos.x >= screenWidth * 0.5)   b.pos.x -= screenWidth;
+        while(b.pos.x <= -screenWidth * 0.5)  b.pos.x += screenWidth;
+        while(b.pos.y >= screenHeight * 0.5)  b.pos.y -= screenHeight;
+        while(b.pos.y <= -screenHeight * 0.5) b.pos.y += screenHeight;
+    }
+}
+static void updateRocks(double dt)
+{
+    float screenWidth = GetScreenWidth();
+    float screenHeight = GetScreenHeight();
+
+    if(rocks.size() < 10)
+    {
+        rocks.emplace_back(Rock{
+            .pos = Vector2{screenWidth * randomFloat(), screenHeight * randomFloat()},
+            .speed = Vector2{
+                (randomFloat() * 2.0f - 1.0f),
+                (randomFloat() * 2.0f - 1.0f)} * MAX_ROCK_SPEED
+        });
+    }
+
+    for(int i = rocks.size() - 1; i >= 0; --i)
+    {
+        Rock& r = rocks[i];
+        r.pos += r.speed * dt;
+        while(r.pos.x >= screenWidth * 0.5)   r.pos.x -= screenWidth;
+        while(r.pos.x <= -screenWidth * 0.5)  r.pos.x += screenWidth;
+        while(r.pos.y >= screenHeight * 0.5)  r.pos.y -= screenHeight;
+        while(r.pos.y <= -screenHeight * 0.5) r.pos.y += screenHeight;
+
+        for(const Bullet& b : bullets)
+        {
+            Vector2 diff = r.pos - b.pos;
+            float d = Vector2DotProduct(diff, diff);
+            if(d < 125.0f)
+            {
+                listSwapRemove(rocks, i);
+                break;
+            }
+        }
+
+    }
+
+}
+
+void render()
+{
+    BeginDrawing();
+
+    ClearBackground({0, 64, 96, 0});
+
+    int32_t screenWidth = GetScreenWidth();
+    int32_t screenHeight = GetScreenHeight();
+
+    drawGrid(screenWidth, screenHeight);
+    drawPlayer(screenWidth, screenHeight);
+    drawBullets(screenWidth, screenHeight);
+    drawRocks(screenWidth, screenHeight);
+
+    DrawFPS(4, 4);
+
+    EndDrawing();
+}
+
+
+
+
+
+void drawGrid(int32_t screenWidth, int32_t screenHeight)
+{
+    const static int32_t VerticalLines = 31;
+    const static int32_t HorizontalLines = 31;
+
+    for(int i = 1; i <= VerticalLines; ++i)
+    {
+        int32_t x = (screenWidth * i / (VerticalLines + 1));
+        DrawLine(x, 0, x, screenHeight, BLACK);
+    }
+
+    for(int i = 1; i <= HorizontalLines; ++i)
+    {
+        int32_t y = (screenHeight * i / (HorizontalLines + 1));
+        DrawLine(0, y, screenWidth, y, BLACK);
+    }
 }
 
 void drawPlayer(int32_t screenWidth, int32_t screenHeight)
@@ -292,41 +363,4 @@ void drawRocks(int32_t screenWidth, int32_t screenHeight)
     }
 }
 
-
-void render()
-{
-    BeginDrawing();
-
-    ClearBackground({0, 64, 96, 0});
-
-    int32_t screenWidth = GetScreenWidth();
-    int32_t screenHeight = GetScreenHeight();
-
-    const static int32_t VerticalLines = 31;
-    const static int32_t HorizontalLines = 31;
-
-    for(int i = 1; i <= VerticalLines; ++i)
-    {
-        int32_t x = (screenWidth * i / (VerticalLines + 1));
-        DrawLine(x, 0, x, screenHeight, BLACK);
-    }
-
-    for(int i = 1; i <= HorizontalLines; ++i)
-    {
-        int32_t y = (screenHeight * i / (HorizontalLines + 1));
-        DrawLine(0, y, screenWidth, y, BLACK);
-    }
-
-
-    drawPlayer(screenWidth, screenHeight);
-    drawBullets(screenWidth, screenHeight);
-    drawRocks(screenWidth, screenHeight);
-
-
-    DrawFPS(4, 4);
-
-
-    EndDrawing();
-
-}
 
